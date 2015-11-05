cfd8103e81fac91ddd8ccd5021d1e6348c660187
Martin Czygan <martin.czygan@gmail.com>
2015-11-05 22:21:36 +0100

    update LICENSE

--- common.go
+++ common.go
@@ -1,13 +1,23 @@
-// Package oaimi implements a few helpers to mirror OAI repositories. The Open
-// Archives Initiative Protocol for Metadata Harvesting (OAI-PMH) is a low-
-// barrier mechanism for repository interoperability.
+//  Copyright 2015 by Leipzig University Library, http://ub.uni-leipzig.de
+//                 by The Finc Authors, http://finc.info
+//                 by Martin Czygan, <martin.czygan@uni-leipzig.de>
 //
-// This project aims to make it simple to create a local, single file view of the repository metadata.
-// It comes with a command line tool, called `oaimi`.
+// This file is part of some open source application.
 //
-// Basic usage:
+// Some open source application is free software: you can redistribute
+// it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
 //
-//     $ oaimi http://digitalcommons.unmc.edu/do/oai/ > metadata.xml
+// Some open source application is distributed in the hope that it will
+// be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
 //
 package oaimi
 
---
+++ doc.go
@@ -0,0 +1,13 @@
+//
+// Package oaimi implements a few helpers to mirror OAI repositories. The Open
+// Archives Initiative Protocol for Metadata Harvesting (OAI-PMH) is a low-
+// barrier mechanism for repository interoperability.
+//
+// This project aims to make it simple to create a local, single file view of the repository metadata.
+// It comes with a command line tool, called `oaimi`.
+//
+// Basic usage:
+//
+//     $ oaimi http://digitalcommons.unmc.edu/do/oai/ > metadata.xml
+//
+package oaimi
