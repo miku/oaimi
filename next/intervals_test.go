810abf9c8037ee7fcb8811b59547c86a68eaaaa3
Martin Czygan <martin.czygan@gmail.com>
2015-11-28 15:15:50 +0100

    next: test Monthly, Weekly interval generation

---
+++ next/intervals_test.go
@@ -0,0 +1,135 @@
+package next
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestWindowMonthly(t *testing.T) {
+	var tests = []struct {
+		w   Window
+		ws  []Window
+		err error
+	}{
+		{
+			w: Window{From: time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC), Until: time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC)},
+			ws: []Window{
+				Window{
+					From:  time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC),
+					Until: time.Date(2000, 1, 1, 23, 59, 59, 999999999, time.UTC),
+				},
+			},
+			err: nil,
+		},
+		{
+			w: Window{From: time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC), Until: time.Date(2000, 5, 1, 0, 0, 0, 0, time.UTC)},
+			ws: []Window{
+				Window{
+					From:  time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC),
+					Until: time.Date(2000, 1, 31, 23, 59, 59, 999999999, time.UTC),
+				},
+				Window{
+					From:  time.Date(2000, 2, 1, 0, 0, 0, 0, time.UTC),
+					Until: time.Date(2000, 2, 29, 23, 59, 59, 999999999, time.UTC),
+				},
+				Window{
+					From:  time.Date(2000, 3, 1, 0, 0, 0, 0, time.UTC),
+					Until: time.Date(2000, 3, 31, 23, 59, 59, 999999999, time.UTC),
+				},
+				Window{
+					From:  time.Date(2000, 4, 1, 0, 0, 0, 0, time.UTC),
+					Until: time.Date(2000, 4, 30, 23, 59, 59, 999999999, time.UTC),
+				},
+				Window{
+					From:  time.Date(2000, 5, 1, 0, 0, 0, 0, time.UTC),
+					Until: time.Date(2000, 5, 1, 23, 59, 59, 999999999, time.UTC),
+				},
+			},
+			err: nil,
+		},
+		{
+			w: Window{From: time.Date(2001, 12, 11, 9, 0, 0, 0, time.UTC), Until: time.Date(2002, 1, 16, 12, 0, 0, 0, time.UTC)},
+			ws: []Window{
+				Window{
+					From:  time.Date(2001, 12, 11, 0, 0, 0, 0, time.UTC),
+					Until: time.Date(2001, 12, 31, 23, 59, 59, 999999999, time.UTC),
+				},
+				Window{
+					From:  time.Date(2002, 1, 1, 0, 0, 0, 0, time.UTC),
+					Until: time.Date(2002, 1, 16, 23, 59, 59, 999999999, time.UTC),
+				},
+			},
+			err: nil,
+		},
+	}
+
+	for _, test := range tests {
+		result, err := test.w.Monthly()
+		if err != test.err {
+			t.Errorf("Monthly() got %v, want %v", err, test.err)
+		}
+		if !reflect.DeepEqual(result, test.ws) {
+			t.Errorf("Monthly() got %v, want %v", result, test.ws)
+		}
+	}
+}
+
+func TestWindowWeekly(t *testing.T) {
+	var tests = []struct {
+		w   Window
+		ws  []Window
+		err error
+	}{
+		{
+			w: Window{From: time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC), Until: time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC)},
+			ws: []Window{
+				Window{
+					From:  time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC),
+					Until: time.Date(2000, 1, 1, 23, 59, 59, 999999999, time.UTC),
+				},
+			},
+			err: nil,
+		},
+		{
+			w: Window{From: time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC), Until: time.Date(2000, 2, 1, 0, 0, 0, 0, time.UTC)},
+			ws: []Window{
+				Window{
+					From:  time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC),
+					Until: time.Date(2000, 1, 1, 23, 59, 59, 999999999, time.UTC),
+				},
+				Window{
+					From:  time.Date(2000, 1, 2, 0, 0, 0, 0, time.UTC),
+					Until: time.Date(2000, 1, 8, 23, 59, 59, 999999999, time.UTC),
+				},
+				Window{
+					From:  time.Date(2000, 1, 9, 0, 0, 0, 0, time.UTC),
+					Until: time.Date(2000, 1, 15, 23, 59, 59, 999999999, time.UTC),
+				},
+				Window{
+					From:  time.Date(2000, 1, 16, 0, 0, 0, 0, time.UTC),
+					Until: time.Date(2000, 1, 22, 23, 59, 59, 999999999, time.UTC),
+				},
+				Window{
+					From:  time.Date(2000, 1, 23, 0, 0, 0, 0, time.UTC),
+					Until: time.Date(2000, 1, 29, 23, 59, 59, 999999999, time.UTC),
+				},
+				Window{
+					From:  time.Date(2000, 1, 30, 0, 0, 0, 0, time.UTC),
+					Until: time.Date(2000, 2, 1, 23, 59, 59, 999999999, time.UTC),
+				},
+			},
+			err: nil,
+		},
+	}
+
+	for _, test := range tests {
+		result, err := test.w.Weekly()
+		if err != test.err {
+			t.Errorf("Weekly() got %v, want %v", err, test.err)
+		}
+		if !reflect.DeepEqual(result, test.ws) {
+			t.Errorf("Weekly() got %v, want %v", result, test.ws)
+		}
+	}
+}
