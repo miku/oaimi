641984169ecf7114749d7efd55362e05bc57eb80
Martin Czygan <martin.czygan@gmail.com>
2015-11-29 16:04:59 +0100

    next: cleanup cache

--- next/cmd/main.go
+++ next/cmd/main.go
@@ -20,16 +20,16 @@ func main() {
 
 	req := next.Request{
 		// Endpoint: "http://export.arxiv.org/oai2",
-		Endpoint: "http://www.librelloph.com/oai",
-		// Endpoint: "http://www.ssoar.info/OAIHandler/request",
+		// Endpoint: "http://www.librelloph.com/oai",
+		Endpoint: "http://www.ssoar.info/OAIHandler/request",
 		// Endpoint: "http://journals.sub.uni-hamburg.de/giga/afsp/oai",
 		// Endpoint: "http://www.doabooks.org/oai",
 		Verb: "ListRecords",
 		// Verb: "ListSets",
 		Prefix: "oai_dc",
 		// Prefix: "marcxml",
-		From:  time.Date(2015, 11, 1, 0, 0, 0, 0, time.UTC),
-		Until: time.Date(2015, 11, 10, 0, 0, 0, 0, time.UTC),
+		From: time.Date(2014, 11, 1, 0, 0, 0, 0, time.UTC),
+		// Until: time.Date(2015, 11, 10, 0, 0, 0, 0, time.UTC),
 	}
 	err := client.Do(req)
 	if err != nil {
